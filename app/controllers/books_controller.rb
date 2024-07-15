8bc4bdf5b3d538946027280872966733022cb03b
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2024-07-15 03:54:03 +0000

    [Add]create機能まで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end 
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get '/top' => 'homes#top'
+  post 'books' => 'books#create'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
