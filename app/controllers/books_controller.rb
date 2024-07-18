c0aa171e4eecf74db06e122b5e2d521fabbd49e7
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2024-07-18 16:53:36 +0000

    [Remove]リンクの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
  def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book)
   end  
   
   def index
@@ -14,12 +15,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end 
