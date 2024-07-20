041c2118c172a85c62971ba9b8f69be774cbca27
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2024-07-20 13:00:19 +0000

    [Add]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
 
+  
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash[:notice] = "・Title can't be blank<br>・Body can't be blank"
-      render :new
+      @books = Book.all
+      render :index
     end
   end
   
@@ -25,13 +26,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash[:notice] = "・Title can't be blank<br>・Body can't be blank"
-      render :new
+      @books = Book.all
+      render :edit
     end
   end
   
